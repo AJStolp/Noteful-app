240f4038d5f3c54a65ae7381238d1490c80d6161
Anthony Stolp <Anthony.Stolp@gmail.com>
2020-02-12 08:25:58 -0600

    pushing before work - implementing components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
+// import {Route} from 'react-router-dom';
+import Header from './header';
+import { render } from '@testing-library/react';
 
-import './App.css';
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
 
-function App() {
-  return (
-    <div className="App">
-   
-    </div>
-  );
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+          <Header />
+      </div>
+    );
+
+  }
 }
 
 export default App;
---
+++ src/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './header.css'
+
+
+const header = () => {
+    return (
+        <header>
+            <h1 className='header'>Noteful</h1>
+        </header>
+    )
+}
+
+export default header
